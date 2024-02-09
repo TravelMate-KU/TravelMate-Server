5a99ac33f8280d006500d8c215b050614cb58973
david-parkk <ji0513ji@naver.com>
2024-02-10 03:16:15 +0900

    feat 테스트 api 추가 및 해당 template 추가

---
+++ src/main/java/konkuk/travelmate/controller/TestController.java
@@ -0,0 +1,14 @@
+package konkuk.travelmate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+
+    @GetMapping("/")
+    public String getHome(){
+        return "/home";
+    }
+}
