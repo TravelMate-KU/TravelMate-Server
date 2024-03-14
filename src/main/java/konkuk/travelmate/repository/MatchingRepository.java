0fb892389abb2222e2fa5a6e6942547dd833f91e
jaeuk520 <jaeuk520@naver.com>
2024-03-14 15:15:51 +0900

    feat: matching 결과 조회 API 구현

--- src/main/java/konkuk/travelmate/repository/MatchingRepository.java
+++ src/main/java/konkuk/travelmate/repository/MatchingRepository.java
@@ -1,7 +1,22 @@
 package konkuk.travelmate.repository;
 
 import konkuk.travelmate.domain.Matching;
+import konkuk.travelmate.form.response.VolunteerMatchingResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+import static javax.management.Query.in;
 
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+    @Query("SELECT new konkuk.travelmate.form.response.VolunteerMatchingResponse(d.name, r.startTime, r.endTime, r.type, r.state, d.phoneNum, d.email) " +
+            "FROM Request r JOIN r.disabled d " +
+            "WHERE d.role = 0 and r.requestId in ( " +
+            "SELECT m.request.requestId " +
+            "From Matching m join m.volunteer v " +
+            "Where v.email = :email)")
+    List<VolunteerMatchingResponse> findVolunteerMatchingResultsByEmail(@Param("email") String email);
 }
--- src/main/java/konkuk/travelmate/service/MatchingService.java
+++ src/main/java/konkuk/travelmate/service/MatchingService.java
@@ -1,9 +1,12 @@
 package konkuk.travelmate.service;
 
+import konkuk.travelmate.form.response.VolunteerMatchingResponse;
 import konkuk.travelmate.repository.MatchingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,4 +15,8 @@ public class MatchingService {
 
     private final MatchingRepository matchingRepository;
 
+    public List<VolunteerMatchingResponse> getMatchResults(String email) {
+        log.info("[MatchingService.getMatchResults]");
+        return matchingRepository.findVolunteerMatchingResultsByEmail(email);
+    }
 }
